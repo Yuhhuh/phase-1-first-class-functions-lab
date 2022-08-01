03fd34500b109f147c3043829f168d8abe360f4c
Mikhail <mikhailtwere@gmail.com>
2022-08-01 01:08:41 -0600

    commit tree 093c1efe7c86cc9ec386ea0ca38bfa3b555e5e57

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function returnFirstTwoDrivers(){
+    let drivers = newDrivers;
+    newDrivers.slice(0,3);
+    return newDrivers;
+}
